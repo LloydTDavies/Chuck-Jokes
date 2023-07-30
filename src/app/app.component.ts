7b6fa81088b37e1b3873d638ca8f078d774f865c
Lloyd Davies <lloydtdaviesit@gmail.com>
2023-07-30 09:59:22 +0100

    Removed app component test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'chuck-jokes';
-}
+export class AppComponent {}
